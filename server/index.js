ba713e3c2cc2e4f598f7d5516776841fc3bfd477
Jonathan Taylor <jonyonson@icloud.com>
2019-08-16 20:31:55 -0500

    remove backend files

--- server/index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./api/server.js');
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, function() {
-  console.log(`\n *** API is running on localhost:${PORT} *** \n`);
-});
