4790b3a5375a2738e74710133142762007b765d0
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-02-24 14:57:54 -0600

    remove unecessary fragment

--- src/components/date-button.js
+++ src/components/date-button.js
@@ -6,18 +6,14 @@ import 'react-datepicker/dist/react-datepicker.css';
 // https://github.com/Hacker0x01/react-datepicker/issues/862
 class DateButton extends React.Component {
   render() {
-    const Button = styled.button`
-      font-size: 14px;
-      height: 30px;
-      margin-top: 10px;
-    `;
-
-    return (
-      <>
-        <Button onClick={this.props.onClick}>{this.props.value}</Button>
-      </>
-    );
+    return <Button onClick={this.props.onClick}>{this.props.value}</Button>;
   }
 }
 
 export default DateButton;
+
+const Button = styled.button`
+  font-size: 14px;
+  height: 30px;
+  margin-top: 10px;
+`;
